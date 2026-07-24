d2b8d6d69e7d99cc046df8013445ef4636386dab
Hannah Humaira Binti Darhaysyam Al Jefri <hbinti-d@u82z04s03.42kl.edu.my>
2026-07-24 20:51:37 +0800

    r01

--- ex00/solve.c
+++ ex00/solve.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   solve.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: likhye-y <likhye-y@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*   By: hbinti-d <hbinti-d@student.42kl.edu.m      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/24 16:33:17 by likhye-y          #+#    #+#             */
-/*   Updated: 2026/07/24 17:49:45 by likhye-y         ###   ########.fr       */
+/*   Created: 2026/07/24 19:09:41 by hbinti-d          #+#    #+#             */
+/*   Updated: 2026/07/24 20:50:40 by hbinti-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,28 @@
 
 int	solve(int grid[4][4], int input[16], int pos)
 {
-    // meow
+	int	row;
+	int	col;
+	int	num;
+
+	if (pos == 16)
+		return (1);
+	row = pos / 4;
+	col = pos % 4;
+	num = 1;
+	while (num >= 1 && num <= 4)
+	{
+		if (duplicate(grid, row, col, num) == 0)
+		{
+			grid[row][col] = num;
+			if (check_all_vis(grid, input, row, col) == 1)
+			{
+				if (solve(grid, input, pos + 1) == 1)
+					return (1);
+			}
+			grid[row][col] = 0;
+		}
+		num++;
+	}
+	return (0);
 }
